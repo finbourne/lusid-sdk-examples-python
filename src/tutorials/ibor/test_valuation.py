c65e9297b40df0267d07af323e45f3fe0cd8c262
Adam Massoud <adam.massoud@finbourne.com>
2023-06-26 09:18:37 +0100

    CTECH-3186: migrate valuation tests to pytest and asyncio

--- src/tutorials/ibor/test_valuation.py
+++ src/tutorials/ibor/test_valuation.py
@@ -1,145 +1,152 @@
-# import unittest
-# from datetime import datetime
+from datetime import datetime
 
-# import pytz
+import pytz
 
-# import lusid
-# import lusid.models as models
-# from lusidfeature import lusid_feature
+import lusid.models as models
+from lusidfeature import lusid_feature
 
-# from utilities import InstrumentLoader, IdGenerator
-# from utilities import TestDataUtilities
-# from utilities.id_generator_utilities import delete_entities
+from utilities import DataUtilities
+import pytest
 
 
-# class Valuation(unittest.TestCase):
+@pytest.fixture(scope="class")
+def default_scope(scope="class"):
+    return DataUtilities.tutorials_scope
 
-#     @classmethod
-#     def setUpClass(cls):
-#         # create a configured API client
-#         api_client = TestDataUtilities.api_client()
 
-#         cls.transaction_portfolios_api = lusid.TransactionPortfoliosApi(api_client)
-#         cls.instruments_api = lusid.InstrumentsApi(api_client)
-#         cls.recipes_api = lusid.ConfigurationRecipeApi(api_client)
-#         cls.aggregation_api = lusid.AggregationApi(api_client)
-#         cls.quotes_api = lusid.QuotesApi(api_client)
-#         cls.portfolios_api = lusid.PortfoliosApi(api_client)
+class TestValuation:
+    @pytest.mark.asyncio
+    @lusid_feature("F10-5")
+    async def test_portfolio_aggregation(
+        self,
+        data_utilities,
+        id_generator,
+        instruments,
+        transaction_portfolios_api,
+        quotes_api,
+        recipes_api,
+        aggregation_api,
+    ):
+        effective_date = datetime(2019, 4, 15, tzinfo=pytz.utc).isoformat()
 
-#         instrument_loader = InstrumentLoader(cls.instruments_api)
-#         cls.instrument_ids = instrument_loader.load_instruments()
+        portfolio_code = await data_utilities.create_transaction_portfolio(
+            DataUtilities.tutorials_scope
+        )
+        id_generator.add_scope_and_code(
+            "portfolio", DataUtilities.tutorials_scope, portfolio_code
+        )
 
-#         cls.test_data_utilities = TestDataUtilities(cls.transaction_portfolios_api)
-#         cls.id_generator = IdGenerator(scope=TestDataUtilities.tutorials_scope)
+        transactions = [
+            data_utilities.build_transaction_request(
+                instrument_id=instruments[0],
+                units=100,
+                price=101,
+                currency="GBP",
+                trade_date=effective_date,
+                transaction_type="StockIn",
+            ),
+            data_utilities.build_transaction_request(
+                instrument_id=instruments[1],
+                units=100,
+                price=102,
+                currency="GBP",
+                trade_date=effective_date,
+                transaction_type="StockIn",
+            ),
+            data_utilities.build_transaction_request(
+                instrument_id=instruments[2],
+                units=100,
+                price=103,
+                currency="GBP",
+                trade_date=effective_date,
+                transaction_type="StockIn",
+            ),
+        ]
 
-#     @classmethod
-#     def tearDownClass(cls):
-#         delete_entities(cls.id_generator)
+        await transaction_portfolios_api.upsert_transactions(
+            scope=DataUtilities.tutorials_scope,
+            code=portfolio_code,
+            transaction_request=transactions,
+        )
 
-#     @lusid_feature("F10-5")
-#     def test_portfolio_aggregation(self):
+        prices = [
+            (instruments[0], 100),
+            (instruments[1], 200),
+            (instruments[2], 300),
+        ]
 
-#         effective_date = datetime(2019, 4, 15, tzinfo=pytz.utc).isoformat()
+        requests = [
+            models.UpsertQuoteRequest(
+                quote_id=models.QuoteId(
+                    quote_series_id=models.QuoteSeriesId(
+                        provider="DataScope",
+                        instrument_id=price[0],
+                        instrument_id_type="LusidInstrumentId",
+                        quote_type="Price",
+                        field="mid",
+                    ),
+                    effective_at=effective_date,
+                ),
+                metric_value=models.MetricValue(value=price[1], unit="GBP"),
+            )
+            for price in prices
+        ]
 
-#         portfolio_code = self.test_data_utilities.create_transaction_portfolio(TestDataUtilities.tutorials_scope)
-#         self.id_generator.add_scope_and_code("portfolio", TestDataUtilities.tutorials_scope, portfolio_code)
+        await quotes_api.upsert_quotes(
+            DataUtilities.tutorials_scope,
+            request_body={
+                "quote" + str(request_number): requests[request_number]
+                for request_number in range(len(requests))
+            },
+        )
 
-#         transactions = [
-#             self.test_data_utilities.build_transaction_request(instrument_id=self.instrument_ids[0],
-#                                                                units=100,
-#                                                                price=101,
-#                                                                currency="GBP",
-#                                                                trade_date=effective_date,
-#                                                                transaction_type="StockIn"),
-#             self.test_data_utilities.build_transaction_request(instrument_id=self.instrument_ids[1],
-#                                                                units=100,
-#                                                                price=102,
-#                                                                currency="GBP",
-#                                                                trade_date=effective_date,
-#                                                                transaction_type="StockIn"),
-#             self.test_data_utilities.build_transaction_request(instrument_id=self.instrument_ids[2],
-#                                                                units=100,
-#                                                                price=103,
-#                                                                currency="GBP",
-#                                                                trade_date=effective_date,
-#                                                                transaction_type="StockIn")
-#         ]
+        recipe_scope = "cs-tutorials"
+        recipe_code = "quotes_recipe"
 
-#         self.transaction_portfolios_api.upsert_transactions(scope=TestDataUtilities.tutorials_scope,
-#                                                             code=portfolio_code,
-#                                                             transaction_request=transactions)
+        id_generator.add_scope_and_code("recipe", recipe_scope, recipe_code)
 
-#         prices = [
-#             (self.instrument_ids[0], 100),
-#             (self.instrument_ids[1], 200),
-#             (self.instrument_ids[2], 300)
-#         ]
+        demo_recipe = models.ConfigurationRecipe(
+            scope=recipe_scope,
+            code=recipe_code,
+            market=models.MarketContext(
+                market_rules=[],
+                suppliers=models.MarketContextSuppliers(equity="DataScope"),
+                options=models.MarketOptions(
+                    default_supplier="DataScope",
+                    default_instrument_code_type="LusidInstrumentId",
+                    default_scope=DataUtilities.tutorials_scope,
+                ),
+            ),
+        )
+        upsert_recipe_request = models.UpsertRecipeRequest(
+            configuration_recipe=demo_recipe
+        )
+        await recipes_api.upsert_configuration_recipe(upsert_recipe_request)
 
-#         requests = [
-#             models.UpsertQuoteRequest(
-#                 quote_id=models.QuoteId(
-#                     models.QuoteSeriesId(
-#                         provider="DataScope",
-#                         instrument_id=price[0],
-#                         instrument_id_type="LusidInstrumentId",
-#                         quote_type="Price",
-#                         field="mid"
-#                     ),
-#                     effective_at=effective_date
-#                 ),
-#                 metric_value=models.MetricValue(
-#                     value=price[1],
-#                     unit="GBP"
-#                 )
-#             )
-#             for price in prices
-#         ]
+        valuation_request = models.ValuationRequest(
+            recipe_id=models.ResourceId(scope=recipe_scope, code=recipe_code),
+            metrics=[
+                models.AggregateSpec(key="Instrument/default/Name", op="Value"),
+                models.AggregateSpec(key="Valuation/PV", op="Proportion"),
+                models.AggregateSpec(key="Valuation/PV", op="Sum"),
+            ],
+            group_by=["Instrument/default/Name"],
+            portfolio_entity_ids=[
+                models.PortfolioEntityId(
+                    scope=DataUtilities.tutorials_scope, code=portfolio_code
+                )
+            ],
+            valuation_schedule=models.ValuationSchedule(effective_at=effective_date),
+        )
+        val_r_json = valuation_request.to_json()
 
-#         self.quotes_api.upsert_quotes(TestDataUtilities.tutorials_scope,
-#                                       request_body={"quote" + str(request_number): requests[request_number]
-#                                                     for request_number in range(len(requests))})
+        #   do the aggregation
+        aggregation = await aggregation_api.get_valuation(
+            valuation_request=valuation_request
+        )
 
-#         recipe_scope = 'cs-tutorials'
-#         recipe_code = 'quotes_recipe'
-
-#         self.id_generator.add_scope_and_code("recipe", recipe_scope, recipe_code)
-
-#         demo_recipe = models.ConfigurationRecipe(
-#             scope=recipe_scope,
-#             code=recipe_code,
-#             market=models.MarketContext(
-#                 market_rules=[],
-#                 suppliers=models.MarketContextSuppliers(
-#                     equity='DataScope'
-#                 ),
-#                 options=models.MarketOptions(
-#                     default_supplier='DataScope',
-#                     default_instrument_code_type='LusidInstrumentId',
-#                     default_scope=TestDataUtilities.tutorials_scope)
-#             )
-#         )
-#         upsert_recipe_request = models.UpsertRecipeRequest(demo_recipe)
-#         self.recipes_api.upsert_configuration_recipe(upsert_recipe_request)
-
-#         valuation_request = models.ValuationRequest(
-#             recipe_id=models.ResourceId(scope=recipe_scope,code=recipe_code),
-#             metrics=[
-#                 models.AggregateSpec("Instrument/default/Name", "Value"),
-#                 models.AggregateSpec("Valuation/PV", "Proportion"),
-#                 models.AggregateSpec("Valuation/PV", "Sum")
-#             ],
-#             group_by=["Instrument/default/Name"],
-#             portfolio_entity_ids=[
-#                 models.PortfolioEntityId(scope=TestDataUtilities.tutorials_scope, code=portfolio_code)
-#             ],
-#             valuation_schedule=models.ValuationSchedule(effective_at=effective_date)
-#         )
-
-#         #   do the aggregation
-#         aggregation = self.aggregation_api.get_valuation(valuation_request=valuation_request)
-
-#         # Asserts
-#         self.assertEqual(len(aggregation.data), 3)
-#         self.assertEqual(aggregation.data[0]["Sum(Valuation/PV)"], 10000)
-#         self.assertEqual(aggregation.data[1]["Sum(Valuation/PV)"], 20000)
-#         self.assertEqual(aggregation.data[2]["Sum(Valuation/PV)"], 30000)
+        # Asserts
+        assert len(aggregation.data) == 3
+        assert aggregation.data[0]["Sum(Valuation/PV)"] == 10000
+        assert aggregation.data[1]["Sum(Valuation/PV)"] == 20000
+        assert aggregation.data[2]["Sum(Valuation/PV)"] == 30000
